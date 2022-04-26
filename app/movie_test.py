5d635e8fd9e36b7d3405102dd02430b7b81f69bc
vionaosiako <viona.osiako@student.morigaschool.com>
2022-04-26 19:36:20 +0300

    Test if the object is initialized properly

--- app/movie_test.py
+++ app/movie_test.py
@@ -13,20 +13,22 @@ class MovieTest(unittest.TestCase):
         '''
         self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
 
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_movie.id,1234)
+        self.assertEqual(self.new_movie.title,"Python Must Be Crazy")
+        self.assertEqual(self.new_movie.overview,"A thrilling new Python Series")
+        # self.assertEqual(self.new_movie.poster,"https://image.tmdb.org/t/p/w500/khsjha27hbs")
+        self.assertEqual(self.new_movie.vote_average,8.5)
+        self.assertEqual(self.new_movie.vote_count,129993)
+
+    
     def test_instance(self):
         self.assertTrue(isinstance(self.new_movie,Movie))
         
-    # def test_init(self):
-    #     '''
-    #     test_init test case to test if the object is initialized properly
-    #     '''
-    #     self.assertEqual(self.new_movie.id,1234)
-    #     self.assertEqual(self.new_movie.title,"Python Must Be Crazy")
-    #     self.assertEqual(self.new_movie.overview,"A thrilling new Python Series")
-    #     self.assertEqual(self.new_movie.poster,"https://image.tmdb.org/t/p/w500/khsjha27hbs")
-    #     self.assertEqual(self.new_movie.vote_average,8.5)
-    #     self.assertEqual(self.new_movie.vote_count,129993)
-
+    
 
 if __name__ == '__main__':
     unittest.main()
