0286be338e187395c6de22860d7b1bd0aa5ba6e6
vionaosiako <viona.osiako@student.morigaschool.com>
2022-04-27 13:55:45 +0300

    Add boostrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
@@ -9,3 +10,6 @@ app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
 from app import views
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
--- app/views.py
+++ app/views.py
@@ -1,30 +1,30 @@
 from email import message
 from flask import render_template
 from app import app
+from .request import get_movies,get_movie
 
 # Views
+@app.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+    
+    return render_template('movie.html',title = title,movie = movie)
+
 @app.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Hello world'
-    return render_template('index.html', message = message)
-
-@app.route('/movie/<movie_id>')
-def movie(movie_id):
-
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('movie.html',id = movie_id)
-
-def index():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
